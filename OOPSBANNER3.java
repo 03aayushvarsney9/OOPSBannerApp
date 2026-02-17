891c5ade51c33fcedc5c5936c15f2ff96a52522a
09aayushvarshney3 <av7974@srmist.edu.in>
2026-02-17 13:49:49 +0530

    print oops3

---
+++ OOPSBANNER3.java
@@ -0,0 +1,41 @@
+public class OOPSBannerUC3 {
+
+    public static void main(String[] args) {
+
+        String line1 = String.join(" ",
+                " OOO  ",
+                "PPPP  ",
+                " OOO  ",
+                "PPPP  ");
+
+        String line2 = String.join(" ",
+                "O   O ",
+                "P   P ",
+                "O   O ",
+                "P   P ");
+
+        String line3 = String.join(" ",
+                "O   O ",
+                "PPPP  ",
+                "O   O ",
+                "PPPP  ");
+
+        String line4 = String.join(" ",
+                "O   O ",
+                "P     ",
+                "O   O ",
+                "P     ");
+
+        String line5 = String.join(" ",
+                " OOO  ",
+                "P     ",
+                " OOO  ",
+                "P     ");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+    }
+}
